523a7c7b98596af7f779ac7ea5cbdca2cd1697b5
Lloyd Akresi <lloydakresi03@gmail.com>
2023-03-21 07:21:53 +0000

    complete

--- server/db/models/color.js
+++ server/db/models/color.js
@@ -17,6 +17,18 @@ module.exports = (sequelize, DataTypes) => {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+      validate:{
+        checkLength(value){
+          if (value.length < 2 || value.length > 20){
+            throw new Error("name should contain between 2 and 20 characters");
+          }
+        },
+        noEndingInY(value) {
+          if(value.slice(-1) === 'y') {
+            throw new Error('name must not end in \'y\'');
+          }
+        }
+      }
     }
   }, {
     sequelize,
